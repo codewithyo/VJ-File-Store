9866116ffcb86c5e01c8e3f82a9a2e7c3bd42973
codewithyo <102235878+codewithyo@users.noreply.github.com>
2024-08-01 14:55:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("28575202", ""))
-API_HASH = environ.get("1b67159ec6b7d40313ee851dd76fdb16", "")
+API_ID = int(environ.get("API_ID", "28575202"))
+API_HASH = environ.get("API_HASH", "1b67159ec6b7d40313ee851dd76fdb16")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/8b99ae45f78ba1eed0a52.jpg')).split() # Bot Start Picture
@@ -35,15 +35,15 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv:// mihaja5084:yeIh95RrMkRNZ3It@cluster0 .6voc3fm.mongodb.net/?retryWrites=true&w= majority")
+DB_URI = environ.get("DB_URI", "mongodb+srv://mihaja5084:yeIh95RrMkRNZ3It@cluster0.6voc3fm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "60")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "3600")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002176164478"))
